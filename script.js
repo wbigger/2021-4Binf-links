e2f1fb263750db564ca23841a5d185fd64918ae1
Claudio Capobianco <>
2021-04-10 07:36:59 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const addNewLink = function() {
+  console.log("inside addNewLink");
+  const url = $("#form-url").val();
+  const title = $("#form-title").val();
+  const category = $("#form-category").val();
+  console.log(url);
+  console.log(title);
+  console.log(category);
+
+  const aElem = $("<a>").attr("href",url).text(title);
+  const liElem = $("<li>").append(aElem);
+  $("#link-list").append(liElem);
+
+}
+
+const init = function() {
+  console.log("inside init");
+  $("#form-submit").on("click",addNewLink);
+}
+
+
+$(document).ready(init);
