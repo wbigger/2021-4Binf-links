5ed41529e5e4db62ed086984953639bb76d808b1
Claudio Capobianco <>
2021-04-12 10:18:15 +0000

    [WIP] use sites var as db

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+const sites = [];
+
+const writeSiteList = function(sites) {
+
+}
+
 const addNewSite = function(event) {
   event.preventDefault();
   console.log("inside addNewSite");
   const url = $("#form-url").val();
   const title = $("#form-title").val();
   const category = $("#form-category").val();
-  console.log(url);
-  console.log(title);
-  console.log(category);
+
+  const newSite = {
+    url: url,
+    title: title,
+    category: category
+  };
+
+  sites.append(newSite); // fino a qui OK
+
+  // CONSEGNA: modificare la pagina in modo da funzionare con la nuova lista "sites"
 
   const aElem = $("<a>").attr("href",url).text(title);
   const liElem = $("<li>").append(aElem);
   $("#site-list").append(liElem);
-
 }
 
 const init = function() {
   console.log("inside init");
-  $("#form-submit").on("click",addNewSite);
+  $("form").submit(addNewSite);
 }
 
-
 $(document).ready(init);
+
+
+
+
+  // const a1 = $("a"); // prendo tutti gli elementi con tag a
+  
+  // const a2 = $("<a>"); // CREO un nuovo elemento con tag a
